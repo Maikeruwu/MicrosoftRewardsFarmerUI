239b6ddd4d195d0c32d6f1c5790f6c7118ce8276
Michael Hintermayr <20190002@students.htl-perg.ac.at>
2023-06-04 10:16:50 +0200

    Implemented Accounts

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace MicrosoftRewardsFarmerUI {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+
+            accountComboBox.ItemsSource = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9};
         }
     }
 }
