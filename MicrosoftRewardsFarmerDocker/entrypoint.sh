44b40785fd0a4f61629f012e9192ad144d389304
Michael Hintermayr <20190002@students.htl-perg.ac.at>
2023-12-27 16:45:02 +0100

    Add entrypoint.sh script for Docker container

---
+++ MicrosoftRewardsFarmerDocker/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# install any new dependencies
+pip install --root-user-action=ignore -r requirements.txt > /dev/null
+
+# set display port and dbus env to avoid hanging (https://github.com/joyzoursky/docker-python-chromedriver)
+export DISPLAY=:99
+export DBUS_SESSION_BUS_ADDRESS=/dev/null
+
+# start xvfb
+Xvfb :99 -screen 0 1280x800x8 -nolisten tcp &
+
+# start the script
+exec "$@"
